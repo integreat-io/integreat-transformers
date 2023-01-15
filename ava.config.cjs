1ad5342094b5a133dc0bc6d164d577ef0200ff9f
Kjell-Morten <post@kjellmorten.no>
2023-01-15 20:31:21 +0100

    Update ava config

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extensions: { ts: 'module' },
+  extensions: { js: true, ts: 'module' },
   nodeArguments: ['--loader=ts-node/esm', '--no-warnings'],
   ignoredByWatcher: ['{coverage,dist}/**'],
   files: ['src/**/*.test.ts'],
