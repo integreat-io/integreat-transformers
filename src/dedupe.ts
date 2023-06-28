161beff02e896571743c3b2f06c77a068caac297
GoodGuyGoose <69195309+GoodGuyGoose@users.noreply.github.com>
2023-06-28 12:29:36 +0200

    Add dedupe transformer

---
+++ src/dedupe.ts
@@ -0,0 +1,26 @@
+import type { Transformer } from 'integreat'
+import { isArray, isObject } from './utils/is.js'
+import { getPathOrDefault } from './utils/getters.js'
+
+export interface Props extends Record<string, unknown> {
+  path?: string
+}
+
+const transformer: Transformer = function removeDuplicates(
+  { path = '.' }: Props,
+  _options
+) {
+  const getter = getPathOrDefault(path)
+  return (data: unknown) => {
+    return !isArray(data)
+      ? data
+      : data.filter((element, index) => {
+          const value = getter(element)
+          return value === undefined
+            ? false
+            : data.findIndex((item) => getter(item) === value) === index
+        })
+  }
+}
+
+export default transformer
